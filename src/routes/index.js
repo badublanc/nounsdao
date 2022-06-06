0fc0ff06a21ce341292fc076e6967a5e1bcb0716
Badu Blanc <itsbadu@hey.com>
2022-06-05 20:24:39 -0500

    update api route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 export const get = async () => {
-	const res = await fetch('http://localhost:8787/v1/nounsdao');
+	const res = await fetch('https://api.cloudnouns.com/v1/nounsdao');
 	const data = await res.json();
 
 	return {
